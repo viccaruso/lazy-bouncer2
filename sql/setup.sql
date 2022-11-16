77f7cdcd6507a4ad3b41354b214eee1766bd9787
Victor Caruso <vic.caruso@gmail.com>
2022-11-15 17:53:56 -0800

    add middleware to routes

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,8 +1,8 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users2;
 
-CREATE TABLE users (
+CREATE TABLE users2 (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   first_name TEXT NOT NULL,
   last_name TEXT NOT NULL,
